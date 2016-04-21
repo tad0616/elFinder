b592d78eaac6176609b5bbc98eafc4eec9f25083
Tad <tad0616@gmail.com>
2016-04-21 20:43:40 +0800

    Update elfinder.zh_TW.js

--- js/i18n/elfinder.zh_TW.js
+++ js/i18n/elfinder.zh_TW.js
@@ -1,17 +1,18 @@
 /**
- * Traditional Chinese translation
+ * 正體中文 translation
  * @author Yuwei Chuang <ywchuang.tw@gmail.com>
- * @version 2014-12-19
+ * @author tad0616@gmail.com
+ * @version 2016-04-21
  */
 if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object') {
 	elFinder.prototype.i18.zh_TW = {
-		translator : 'Yuwei Chuang &lt;ywchuang.tw@gmail.com&gt;',
+		translator : 'Yuwei Chuang &lt;ywchuang.tw@gmail.com&gt;, tad0616@gmail.com',
 		language   : '正體中文',
 		direction  : 'ltr',
 		dateFormat : 'M d, Y h:i A', // Mar 13, 2012 05:27 PM
-		fancyDateFormat : '$1 H:i',
+		fancyDateFormat : '$1 H:i', // will produce smth like: Today 12:25 PM
 		messages   : {
-			
+
 			/********************************** errors **********************************/
 			'error'                : '錯誤',
 			'errUnknown'           : '未知的錯誤.',
@@ -50,13 +51,15 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'errRename'            : '不能重新命名 "$1".',
 			'errCopyFrom'          : '不允許從 volume "$1" 複製.',
 			'errCopyTo'            : '不允複製到 volume "$1".',
-			'errUpload'            : '上船錯誤.',
-			'errUploadFile'        : '無法上傳 "$1".',
+			'errMkOutLink'         : '無法在根目錄以外建立連結', // from v2.1 added 03.10.2015
+			'errUpload'            : '上船錯誤.',  // old name - errUploadCommon
+			'errUploadFile'        : '無法上傳 "$1".', // old name - errUpload
 			'errUploadNoFiles'     : '未找到要上傳的檔案.',
-			'errUploadTotalSize'   : '資料超過了最大允許大小.',
-			'errUploadFileSize'    : '檔案超過了最大允許大小.',
+			'errUploadTotalSize'   : '資料超過了最大允許大小.', // old name - errMaxSize
+			'errUploadFileSize'    : '檔案超過了最大允許大小.', //  old name - errFileMaxSize
 			'errUploadMime'        : '不允許的檔案類型.',
-			'errUploadTransfer'    : '"$1" 傳輸錯誤.', 
+			'errUploadTransfer'    : '"$1" 傳輸錯誤.',
+			'errUploadTemp'        : '無法替上傳檔案建立暫存檔', // from v2.1 added 26.09.2015
 			'errNotReplace'        : '"$1" 已經存在此位置, 不能被其他的替换.', // new
 			'errReplace'           : '無法替换 "$1".',
 			'errSave'              : '無法保存 "$1".',
@@ -71,27 +74,32 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'errNoArchive'         : '檔案不是壓縮檔案, 或者不支援該壓缩格式.',
 			'errCmdNoSupport'      : '後端不支援該指令.',
 			'errReplByChild'       : '資料夾 “$1” 不能被它所包含的檔案(資料夾)替换.',
-			'errArcSymlinks'       : '出于安全上的考量，禁止解壓縮檔案包含不允許的檔案名稱.',
+			'errArcSymlinks'       : '出于安全上的考量，禁止解壓縮檔案包含不允許的檔案名稱.', // edited 24.06.2012
 			'errArcMaxSize'        : '壓縮檔案超過最大允許檔案大小範圍.',
 			'errResize'            : '無法重新調整大小 "$1".',
-			'errResizeDegree'      : 'Invalid rotate degree.',
-			'errResizeRotate'      : 'Unable to rotate image.',
-			'errResizeSize'        : 'Invalid image size.',
-			'errResizeNoChange'    : 'Image size not changed.',
+			'errResizeDegree'      : '無效的旋轉角度',  // added 7.3.2013
+			'errResizeRotate'      : '無法旋轉圖片',  // added 7.3.2013
+			'errResizeSize'        : '無效的圖片尺寸',  // added 7.3.2013
+			'errResizeNoChange'    : '圖片尺寸未變動',  // added 7.3.2013
 			'errUsupportType'      : '不支援的檔案格式.',
 			'errNotUTF8Content'    : '檔案 "$1" 不是 UTF-8 格式, 不能編輯.',  // added 9.11.2011
 			'errNetMount'          : '無法掛載 "$1".', // added 17.04.2012
 			'errNetMountNoDriver'  : '不支援該通訊協議.',     // added 17.04.2012
 			'errNetMountFailed'    : '掛載失敗.',         // added 17.04.2012
 			'errNetMountHostReq'   : '需要指定主機位置.', // added 18.04.2012
-			'errSessionExpires'    : 'Your session has expired due to inactivity.',
-			'errCreatingTempDir'   : 'Unable to create temporary directory: "$1"',
-			'errFtpDownloadFile'   : 'Unable to download file from FTP: "$1"',
-			'errFtpUploadFile'     : 'Unable to upload file to FTP: "$1"',
-			'errFtpMkdir'          : 'Unable to create remote directory on FTP: "$1"',
-			'errArchiveExec'       : 'Error while archiving files: "$1"',
-			'errExtractExec'       : 'Error while extracting files: "$1"',
-			
+			'errSessionExpires'    : '太久未動作導致session過期',
+			'errCreatingTempDir'   : '無法建立暫存資料夾： "$1"',
+			'errFtpDownloadFile'   : '無法從FTP下載此檔： "$1"',
+			'errFtpUploadFile'     : '無法將此檔上傳到FTP "$1"',
+			'errFtpMkdir'          : '無法在FTP上建立遠端目錄："$1"',
+			'errArchiveExec'       : '壓縮檔案錯誤："$1"',
+			'errExtractExec'       : '解壓縮檔案錯誤："$1"',
+			'errNetUnMount'        : '無法卸載', // from v2.1 added 30.04.2012
+			'errConvUTF8'          : '無法轉碼為 UTF-8', // from v2.1 added 08.04.2014
+			'errFolderUpload'      : '試試 Google Chrome，假如您想上傳此資料夾', // from v2.1 added 26.6.2015
+			'errSearchTimeout'     : '搜尋 "$1" 超時，只取得局部結果。', // from v2.1 added 12.1.2016
+			'errReauthRequire'     : '必須重新認證', // from v2.1.10 added 3.24.2016
+
 			/******************************* commands names ********************************/
 			'cmdarchive'   : '建立壓縮檔案',
 			'cmdback'      : '後退',
@@ -107,6 +115,7 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'cmdhome'      : '首頁',
 			'cmdinfo'      : '查看關於',
 			'cmdmkdir'     : '建立資料夾',
+			'cmdmkdirin'   : '進入新資料夾', // from v2.1.7 added 19.2.2016
 			'cmdmkfile'    : '建立文字檔案',
 			'cmdopen'      : '打開',
 			'cmdpaste'     : '貼上',
@@ -120,9 +129,13 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'cmdview'      : '查看',
 			'cmdresize'    : '重新調整大小',
 			'cmdsort'      : '排序',
-			'cmdnetmount'  : '掛載 net volume', // added 18.04.2012
-			
-			/*********************************** buttons ***********************************/ 
+			'cmdnetmount'  : '掛載網路磁碟機', // added 18.04.2012
+			'cmdnetunmount': '卸載', // from v2.1 added 30.04.2012
+			'cmdplaces'    : 'To Places', // added 28.12.2014
+			'cmdchmod'     : '變更模式', // from v2.1 added 20.6.2015
+			'cmdopendir'   : '開啟資料夾', // from v2.1 added 13.1.2016
+
+			/*********************************** buttons ***********************************/
 			'btnClose'  : '關閉',
 			'btnSave'   : '儲存',
 			'btnRm'     : '删除',
@@ -131,7 +144,17 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'btnNo'     : '否',
 			'btnYes'    : '是',
 			'btnMount'  : '掛載',  // added 18.04.2012
-			
+			'btnApprove': '轉到$1並批准', // from v2.1 added 26.04.2012
+			'btnUnmount': '卸載', // from v2.1 added 30.04.2012
+			'btnConv'   : '轉碼', // from v2.1 added 08.04.2014
+			'btnCwd'    : '此處',      // from v2.1 added 22.5.2015
+			'btnVolume' : '磁碟',    // from v2.1 added 22.5.2015
+			'btnAll'    : '全部',       // from v2.1 added 22.5.2015
+			'btnMime'   : 'MIME Type', // from v2.1 added 22.5.2015
+			'btnFileName':'檔名',  // from v2.1 added 22.5.2015
+			'btnSaveClose': '儲存並關閉', // from v2.1 added 12.6.2015
+			'btnBackup' : '備份', // fromv2.1 added 28.11.2015
+
 			/******************************** notifications ********************************/
 			'ntfopen'     : '打開資料夾',
 			'ntffile'     : '打開檔案',
@@ -152,9 +175,15 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'ntfresize'   : '正在更改尺寸',
 			'ntfsmth'     : '正在忙 >_<',
 			'ntfloadimg'  : '正在讀取圖片',
-      		'ntfnetmount' : '正在掛載 net volume', // added 18.04.2012
-			'ntfdim'      : 'Acquiring image dimension',
-			
+			'ntfnetmount' : '正在掛載 net volume', // added 18.04.2012
+			'ntfnetunmount': '卸載網路磁碟機中', // from v2.1 added 30.04.2012
+			'ntfdim'      : '獲取圖片尺寸', // added 20.05.2013
+			'ntfreaddir'  : '讀取資料夾資訊', // from v2.1 added 01.07.2013
+			'ntfurl'      : '取得連結位置', // from v2.1 added 11.03.2014
+			'ntfchmod'    : '檔案模式變更中', // from v2.1 added 20.6.2015
+			'ntfpreupload': '驗證上傳的檔案名稱', // from v2.1 added 31.11.2015
+			'ntfzipdl'    : '為下載建立檔案中', // from v2.1.7 added 23.1.2016
+
 			/************************************ dates **********************************/
 			'dateUnknown' : '未知',
 			'Today'       : '今天',
@@ -190,25 +219,32 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'Thursday'    : '星期四',
 			'Friday'      : '星期五',
 			'Saturday'    : '星期六',
-			'Sun'         : '周日', 
-			'Mon'         : '周一', 
-			'Tue'         : '周二', 
-			'Wed'         : '周三', 
-			'Thu'         : '周四', 
-			'Fri'         : '周五', 
+			'Sun'         : '周日',
+			'Mon'         : '周一',
+			'Tue'         : '周二',
+			'Wed'         : '周三',
+			'Thu'         : '周四',
+			'Fri'         : '周五',
 			'Sat'         : '周六',
-			
+
 			/******************************** sort variants ********************************/
-			'sortname'          : '按名稱', 
-			'sortkind'          : '按類型', 
+			'sortname'          : '按名稱',
+			'sortkind'          : '按類型',
 			'sortsize'          : '按大小',
 			'sortdate'          : '按日期',
 			'sortFoldersFirst'  : 'Folders first',
+
+			/********************************** new items **********************************/
+			'untitled file.txt' : 'NewFile.txt', // added 10.11.2015
+			'untitled folder'   : '新資料夾',   // added 10.11.2015
+			'Archive'           : '新文件',  // from v2.1 added 10.11.2015
 
 			/********************************** messages **********************************/
 			'confirmReq'      : '請確認',
 			'confirmRm'       : '確定要删除檔案嗎?<br/>該操作不可回復!',
 			'confirmRepl'     : '用新的檔案替换原有檔案?',
+			'confirmConvUTF8' : '不是UTF-8<br/>是否轉碼到 UTF-8？<br/>內容將在儲存轉檔後轉為 UTF-8', // from v2.1 added 08.04.2014
+			'confirmNotSave'  : '已經被修改！<br/>若沒有存檔將失去剛剛的所有修改', // from v2.1 added 15.7.2015
 			'apllyAll'        : '全部使用',
 			'name'            : '名稱',
 			'size'            : '大小',
@@ -227,7 +263,7 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'viewlist'        : '列表檢視',
 			'viewicons'       : '圖示檢視',
 			'places'          : '位置',
-			'calc'            : '計算', 
+			'calc'            : '計算',
 			'path'            : '路徑',
 			'aliasfor'        : '别名',
 			'locked'          : '鎖定',
@@ -238,7 +274,7 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'yes'             : '是',
 			'no'              : '否',
 			'link'            : '連結',
-			'searcresult'     : '搜尋结果',  
+			'searcresult'     : '搜尋结果',
 			'selected'        : '選取的項目',
 			'about'           : '關於',
 			'shortcuts'       : '快捷鍵',
@@ -248,9 +284,9 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'protocolver'     : '協定版本',
 			'homepage'        : '首頁',
 			'docs'            : '文件',
-			'github'          : 'Fork us on Github',
-			'twitter'         : 'Follow us on twitter',
-			'facebook'        : 'Join us on facebook',
+			'github'          : '在Github上Fork我們',
+			'twitter'         : '在twitter上關注我們',
+			'facebook'        : '在facebook上加入我們',
 			'team'            : '團隊',
 			'chiefdev'        : '首席開發者',
 			'developer'       : '開發者',
@@ -276,13 +312,37 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'rotate-cw'       : '順時針旋轉90度',
 			'rotate-ccw'      : '逆時針旋轉90度',
 			'degree'          : '度',
-			'netMountDialogTitle' : 'Mount network volume', // added 18.04.2012
-			'protocol'            : 'Protocol', // added 18.04.2012
-			'host'                : 'Host', // added 18.04.2012
-			'port'            : '接口', // added 18.04.2012
-			'user'            : '使用者', // added 18.04.2012
-			'pass'            : '密碼', // added 18.04.2012
-			
+			'netMountDialogTitle' : '掛載網路磁碟', // added 18.04.2012
+			'protocol'            : '協定', // added 18.04.2012
+			'host'                : '主機', // added 18.04.2012
+			'port'                : '接口', // added 18.04.2012
+			'user'                : '使用者', // added 18.04.2012
+			'pass'                : '密碼', // added 18.04.2012
+			'confirmUnmount'      : '確定要掛載 $1？',  // from v2.1 added 30.04.2012
+			'dropFilesBrowser': '從瀏覽器拖拉或貼上檔案', // from v2.1 added 30.05.2012
+			'dropPasteFiles'  : '拖拉或貼上檔案及網址至此', // from v2.1 added 07.04.2014
+			'encoding'        : '編碼', // from v2.1 added 19.12.2014
+			'locale'          : '本地',   // from v2.1 added 19.12.2014
+			'searchTarget'    : '目標： $1',                // from v2.1 added 22.5.2015
+			'searchMime'      : '以 MIME Type 搜尋', // from v2.1 added 22.5.2015
+			'owner'           : '擁有者', // from v2.1 added 20.6.2015
+			'group'           : '群組', // from v2.1 added 20.6.2015
+			'other'           : '其他', // from v2.1 added 20.6.2015
+			'execute'         : '執行', // from v2.1 added 20.6.2015
+			'perm'            : '權限', // from v2.1 added 20.6.2015
+			'mode'            : '模式', // from v2.1 added 20.6.2015
+			'emptyFolder'     : '資料夾是空的', // from v2.1.6 added 30.12.2015
+			'emptyFolderDrop' : '資料夾是空的\\A 拖曳上傳', // from v2.1.6 added 30.12.2015
+			'emptyFolderLTap' : '資料夾是空的\\A 長按上傳', // from v2.1.6 added 30.12.2015
+			'quality'         : '品質', // from v2.1.6 added 5.1.2016
+			'autoSync'        : '自動同步',  // from v2.1.6 added 10.1.2016
+			'moveUp'          : '上移',  // from v2.1.6 added 18.1.2016
+			'getLink'         : '取得連結網址', // from v2.1.7 added 9.2.2016
+			'selectedItems'   : '選擇項目 ($1)', // from v2.1.7 added 2.19.2016
+			'folderId'        : '資料夾 ID', // from v2.1.10 added 3.25.2016
+			'offlineAccess'   : '允許離線存取', // from v2.1.10 added 3.25.2016
+			'reAuth'          : ' 重新認證', // from v2.1.10 added 3.25.2016
+
 			/********************************** mimetypes **********************************/
 			'kindUnknown'     : '未知',
 			'kindFolder'      : '資料夾',
@@ -332,6 +392,7 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 			'kindAWK'         : 'AWK 程式碼',
 			'kindCSV'         : '逗號分隔值檔案(CSV)',
 			'kindDOCBOOK'     : 'Docbook XML 文件',
+			'kindMarkdown'    : 'Markdown 文件', // added 20.7.2015
 			// images
 			'kindImage'       : '圖片',
 			'kindBMP'         : 'BMP 圖片',
@@ -364,3 +425,4 @@ if (elFinder && elFinder.prototype && typeof(elFinder.prototype.i18) == 'object'
 		}
 	};
 }
+
